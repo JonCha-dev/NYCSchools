914a921629e12a50dac5593c5a2a811b6e0dc508
Jon Chang <jonasenchang@gmail.com>
2023-07-29 09:35:18 -0700

    Initial Commit

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Jon Chang on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
